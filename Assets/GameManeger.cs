6f2f5886fc4e5520d1ac3a82d8039903b17f9c85
uepon0310 <a_ueha@yahoo.co.jp>
2019-03-11 08:39:17 +0900

    first commit

---
+++ Assets/GameManeger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManeger : MonoBehaviour {
+
+    GameObject hpGauge;
+
+	// Use this for initialization
+	void Start () {
+        this.hpGauge = GameObject.Find("hpGauge");
+
+    }
+
+    public void DecreaseHp()
+    {
+        this.hpGauge.GetComponent<Image>().fillAmount -= 0.1f;
+    }
+
+}
